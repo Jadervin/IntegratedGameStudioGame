317581729a195beb58ecc46c77c9ec07ab91cd43
Jadervin <jdervin@ltu.edu>
2022-01-19 16:55:43 -0500

    Added Menus

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    [Header("Scene Names")]
+    public string StartSceneName;
+    public string CreditsSceneName;
+    public string TitleMenuSceneName;
+
+    [Header("Sound Sources")]
+    public AudioSource soundSource;
+    public AudioClip menuClick;
+    public float clickTimer = 0.5f;
+
+    public void StartButtonPressed()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        soundSource.PlayOneShot(menuClick);
+        StartCoroutine(WaitforStartButton(clickTimer));
+        
+    }
+    
+    public void CreditsButtonPressed()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        soundSource.PlayOneShot(menuClick);
+        StartCoroutine(WaitforCreditsButton(clickTimer));
+       
+
+
+
+    }
+
+    public void TitleMenuButtonPressed()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        soundSource.PlayOneShot(menuClick);
+        StartCoroutine(WaitforTitleMenuButton(clickTimer));
+
+
+
+
+    }
+    public void CloseGame()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        soundSource.PlayOneShot(menuClick);
+        StartCoroutine(WaitforCloseButton(clickTimer));
+
+
+
+    }
+
+    IEnumerator WaitforStartButton(float duration)
+    {
+
+        yield return new WaitForSeconds(duration);   //Wait
+        SceneManager.LoadScene(StartSceneName);
+    }
+    IEnumerator WaitforTitleMenuButton(float duration)
+    {
+
+        yield return new WaitForSeconds(duration);   //Wait
+        SceneManager.LoadScene(TitleMenuSceneName);
+    }
+
+    IEnumerator WaitforCreditsButton(float duration)
+    {
+
+        yield return new WaitForSeconds(duration);   //Wait
+        SceneManager.LoadScene(CreditsSceneName);
+    }
+
+    IEnumerator WaitforCloseButton(float duration)
+    {
+
+        yield return new WaitForSeconds(duration);   //Wait
+        Application.Quit();
+
+    }
+
+
+
+
+
+}
