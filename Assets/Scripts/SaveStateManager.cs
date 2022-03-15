63c6155087401949832adb135668bf4cce23b4b7
Jadervin <jdervin@ltu.edu>
2022-03-14 20:45:44 -0400

    Made a Separate Class for Array
    I made a separate class for the scene Names Array

--- Assets/Scripts/SaveStateManager.cs
+++ Assets/Scripts/SaveStateManager.cs
@@ -11,30 +11,7 @@ public class SaveStateManager : MonoBehaviour
     private DialogueManager dialogueManager;
     string filePath;
 
-    private string[] sceneNames =
-    {
-            "Choose your Name",
-            "Scene 1",
-            "Scene 2",
-            "Scene 2 Battle",
-            "Scene 2 After Battle",
-            "Scene 3",
-            "Excursion Break 1",
-            "Belladonna 1",
-            "Cherry Blossom 1",
-            "Holly 1",
-            "Spider Lily 1",
-            "Ivy 1",
-            "Scene 4",
-            "Scene 5",
-            "Scene 6",
-            "Excursion Break 2",
-            "Aether 1",
-            "Galedric 1",
-            "Xzciar 1",
-            "Scene 6"
-
-    };
+    private SceneNamesScript sceneNames;
 
     static public SaveStateManager instance;
 
---
+++ Assets/Scripts/SceneNamesScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneNamesScript
+{
+    private string[] sceneNames =
+     {
+            "Choose your Name",
+            "Scene 1",
+            "Scene 2",
+            "Scene 2 Battle",
+            "Scene 2 After Battle",
+            "Scene 3",
+            "Excursion Break 1",
+            "Belladonna 1",
+            "Cherry Blossom 1",
+            "Holly 1",
+            "Spider Lily 1",
+            "Ivy 1",
+            "Scene 4",
+            "Scene 5",
+            "Scene 6",
+            "Excursion Break 2",
+            "Aether 1",
+            "Galedric 1",
+            "Xzciar 1",
+            "Scene 6"
+
+    };
+}
