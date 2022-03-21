138b6a8f823f4813e4e32996a2bca00d1a0ac419
Jadervin <jdervin@ltu.edu>
2022-03-20 22:23:12 -0400

    Added Hovering Tips

---
+++ Assets/Scripts/HoverTips.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class HoverTips : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    public string tipToShow;
+    private float timeToWait = 0.2f;
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(StartTimer());
+    }
+
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        HoverTipManager.OnMouseLoseFocus();
+    }
+
+
+    private void ShowMessage()
+    {
+        StopAllCoroutines();
+        HoverTipManager.OnMouseHover(tipToShow, Input.mousePosition);
+
+    }
+
+    private IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(timeToWait);
+
+
+        ShowMessage();
+
+    }
+
+}
